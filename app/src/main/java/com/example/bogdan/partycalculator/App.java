af133c429823f671beddc671396ef775bb7946cf
Bogdan <kolom996@gmail.com>
2016-07-15 14:40:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bogdan/partycalculator/App.java
@@ -0,0 +1,12 @@
+package com.example.bogdan.partycalculator;
+
+import android.app.Application;
+
+/**
+ * @author Bogdan Kolomiets
+ * @version 1
+ * @date 15.07.16
+ */
+public class App extends Application {
+
+}
---
+++ app/src/main/java/com/example/bogdan/partycalculator/model/User.java
@@ -0,0 +1,10 @@
+package com.example.bogdan.partycalculator.model;
+
+/**
+ * @author Bogdan Kolomiets
+ * @version 1
+ * @date 15.07.16
+ */
+public class User {
+
+}
